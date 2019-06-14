c108e807985629cc15d6cd482180805706a5c22a
Carl Hinderer <carl.hinderer4@gmail.com>
2019-06-13 23:30:20 -0600

    Added DoubleNode class and test.

---
+++ code/src/collection/double_node.py
@@ -0,0 +1,8 @@
+class DoubleNode:
+    def __init__(self, data=None, next=None, prev=None):
+        self.data = data
+        self.next = next
+        self.prev = prev
+
+    def __str__(self):
+        return str(self.data)
---
+++ code/test/collection/test_double_node.py
@@ -0,0 +1,13 @@
+from src.collection.double_node import *
+import pytest
+
+class TestDoubleNode:
+    def test_constructor(self):
+        node = DoubleNode('abc')
+        assert node.data == 'abc'
+        assert node.prev is None
+        assert node.next is None
+
+    def test_string_conversion(self):
+        node = DoubleNode(123)
+        assert str(node) == '123'
