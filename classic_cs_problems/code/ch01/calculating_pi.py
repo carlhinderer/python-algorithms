fc6336643e1834916e13fd05a00ac09e705b08af
Carl Hinderer <carl.hinderer4@gmail.com>
2022-01-24 21:20:08 -0700

    Added pi calculation example.

---
+++ classic_cs_problems/code/ch01/calculating_pi.py
@@ -0,0 +1,18 @@
+
+def calculate_pi(n_terms):
+    numerator = 4.0
+    denominator = 1.0
+    operation = 1.0
+    pi = 0.0
+
+    for _ in range(n_terms):
+        pi += operation * (numerator / denominator)
+        denominator += 2.0
+        operation = -operation
+
+    return pi
+
+
+
+if __name__ == '__main__':
+    print(calculate_pi(1000000))
