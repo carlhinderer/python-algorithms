016a197f8217af281081afda3dc5e0aa16d73cd3
Carl Hinderer <carl.hinderer4@gmail.com>
2022-06-28 13:59:52 -0600

    Did Chapter 1, exercises 4-6.

--- pythonds3/code/src/ch01/fraction.py
+++ pythonds3/code/src/ch01/fraction.py
@@ -4,10 +4,20 @@ def gcd(m, n):
     return n
 
 
+class NonIntegerFractionError(Exception):
+    pass
+
+
 class Fraction:
     def __init__(self, top, bottom):
+        if not(type(top) == int and type(bottom) == int):
+            raise NonIntegerFractionError
         if bottom == 0:
             raise ZeroDivisionError
+        if bottom < 0:
+            top = -top
+            bottom = -bottom
+
         cmmn = gcd(top, bottom)
         self.num = top // cmmn
         self.den = bottom // cmmn
@@ -18,8 +28,22 @@ class Fraction:
     def __eq__(self, other_fraction):
         first_num = self.num * other_fraction.den
         second_num = other_fraction.num * self.den
-
         return first_num == second_num
+
+    def __gt__(self, other_fraction):
+        return (self.num / self.den) > (other_fraction.num / other_fraction.den)
+
+    def __ge__(self, other_fraction):
+        return self > other_fraction or self == other_fraction
+
+    def __lt__(self, other_fraction):
+        return (self.num / self.den) < (other_fraction.num / other_fraction.den)
+
+    def __le__(self, other_fraction):
+        return self < other_fraction or self == other_fraction
+
+    def __ne__(self, other_fraction):
+        return not self == other_fraction
 
     def __add__(self, other_fraction):
         new_num = self.num * other_fraction.den + self.den * other_fraction.num
--- pythonds3/code/tests/ch01/test_fraction.py
+++ pythonds3/code/tests/ch01/test_fraction.py
@@ -1,12 +1,31 @@
 import pytest
 
-from src.ch01.fraction import Fraction
+from src.ch01.fraction import Fraction, NonIntegerFractionError
 
 
 def test_initialization():
     f = Fraction(2, 4)
     assert f.get_num() == 1
     assert f.get_den() == 2
+
+def test_integers():
+    with pytest.raises(NonIntegerFractionError):
+        f = Fraction(0.5, 2)
+    with pytest.raises(NonIntegerFractionError):
+        f = Fraction(2, 'abc')
+
+def test_negative_fraction_initialization():
+    f1 = Fraction(-1, 3)
+    f2 = Fraction(1, -3)
+    f3 = Fraction(-1, -3)
+
+    assert f1.get_num() == -1 and f1.get_den() == 3
+    assert f2.get_num() == -1 and f2.get_den() == 3
+    assert f3.get_num() == 1 and f3.get_den() == 3
+
+def test_nonzero_denominator():
+    with pytest.raises(ZeroDivisionError):
+        f = Fraction(2, 0)
 
 def test_get_num():
     f = Fraction(1, 4)
@@ -25,6 +44,16 @@ def test_equality():
     assert f1 == f2
     assert f1 == f3
     assert f1 != f4
+
+def test_comparison_operators():
+    f1 = Fraction(1, 2)
+    f2 = Fraction(2, 3)
+    f3 = Fraction(2, 3)
+
+    assert f2 > f1
+    assert f2 >= f3
+    assert f1 < f2
+    assert f2 <= f3
 
 def test_str():
     f1 = Fraction(1, 4)
