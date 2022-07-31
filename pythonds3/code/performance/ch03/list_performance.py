59475c940aa685a76902e32ad5cf25680b78de68
Carl Hinderer <carl.hinderer4@gmail.com>
2022-07-31 11:31:09 -0600

    Added more performance tests for lists and dictionaries.

--- pythonds3/code/performance/ch03/list_performance.py
+++ pythonds3/code/performance/ch03/list_performance.py
@@ -3,6 +3,7 @@
 from timeit import Timer
 
 
+# List Creation
 # Concatenation
 def test1():
     l = []
@@ -39,6 +40,28 @@ def get_list_creation_performances():
 
     t4 = Timer("test4()", "from __main__ import test4")
     print(f"list range: {t4.timeit(number=1000):18.2f} milliseconds")
+
+
+# Pop Performance
+# Simple comparison
+pop_zero = Timer("x.pop(0)", "from __main__ import x")
+pop_end = Timer("x.pop()", "from __main__ import x")
+
+x = list(range(2000000))
+print(f"pop(0): {pop_zero.timeit(number=1000):10.5f} milliseconds")
+
+x = list(range(2000000))
+print(f"pop(): {pop_end.timeit(number=1000):11.5f} milliseconds")
+
+
+# Test for different list sizes
+print(f"{'n':10s}{'pop(0)':>15s}{'pop()':>15s}")
+for i in range(1_000_000, 100_000_001, 1_000_000):
+    x = list(range(i))
+    pop_zero_t = pop_zero.timeit(number=1000)
+    x = list(range(i))
+    pop_end_t = pop_end.timeit(number=1000)
+    print(f"{i:<10d}{pop_zero_t:>15.5f}{pop_end_t:>15.5f}")
 
 
 if __name__ == '__main__':
